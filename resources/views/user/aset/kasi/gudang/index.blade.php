41818567dab82e2e0d44940d8039a310d4f5b6b7
Dede <deirfed@gmail.com>
2024-04-19 23:48:40 +0700

    fix: revisi front-end

--- resources/views/user/aset/kasi/gudang/index.blade.php
+++ resources/views/user/aset/kasi/gudang/index.blade.php
@@ -65,6 +65,7 @@
                                 </div> --}}
                             </div>
                         </div>
+                        {{-- PAGINASI --}}
                         <div class="col-xl-6 col-lg-6 col-md-6 col-sm-12 col-12">
                             <form class="form-inline mb-2 d-flex justify-content-end">
                                 <input class="form-control mr-sm-2" type="search" placeholder="Cari sesuatu di sini..."
