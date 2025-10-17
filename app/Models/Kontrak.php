163572d4b9270b16d670c67046a5aa0c3657cc4d
Tio Muhamad Nur <outsider.kuningan52@gmail.com>
2025-10-17 16:59:17 +0700

    update: kontrak softdeletes

--- app/Models/Kontrak.php
+++ app/Models/Kontrak.php
@@ -5,10 +5,11 @@ namespace App\Models;
 use Illuminate\Support\Str;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Kontrak extends Model
 {
-    use HasFactory;
+    use HasFactory, SoftDeletes;
 
     protected $table = 'kontrak';
 
